fb342f405c28152b851676ed8c6588e7b16c9054
nxrix <nxrixz@gmail.com>
2026-05-30 18:11:26 +0330

    Update renderer.js

--- assets/js/renderer.js
+++ assets/js/renderer.js
@@ -69,7 +69,7 @@ const getShader = (state) => {
   return shaderRaw.replace("//--input--//",
     (j?"#define julia\n\n":"")+
     (a0?"#define iabsz\n\n":"")+
-    (a1?"#define iabsz\n\n":"")+
+    (a1?"#define iabszn\n\n":"")+
     (!(j||a0||a1)?"#define accurate_n\n\n":"")+
     `${getMul(state)}\n`+
     `#define s(p) p.${["xyzw","xywz","xwyz","wxyz"][state.s]}\n`
